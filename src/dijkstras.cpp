e8c232508ef84b70150bce15716bdaf8c599704e
samdryoo <sdryoo@uci.edu>
2025-03-12 13:17:43 -0700

    Update dijkstras.cpp

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -44,7 +44,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
-        cout << "No path found." << endl;
+        cout << "\nTotal cost is " << total << "\n";
         return;
     }
 
