a4e7217f83ae643314bba9f9a8b5640e0468b36a
samdryoo <sdryoo@uci.edu>
2025-03-12 13:17:57 -0700

    Update ladder.cpp

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -76,11 +76,15 @@ void print_word_ladder(const vector<string>& ladder) {
     cout << endl;
 }
 
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
+
 void verify_word_ladder() {
     set<string> word_list;
     load_words(word_list, "words.txt");
-    generate_word_ladder("cat", "dog", word_list).size() == 4;
-    generate_word_ladder("marty", "curls", word_list).size() == 6;
-    generate_word_ladder("code", "data", word_list).size() == 6;
-    generate_word_ladder("work", "play", word_list).size() == 6;
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
 }
